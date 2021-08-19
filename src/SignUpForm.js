e2e4cb6754026377c9ed641685a34351dc7d35b7
Wukongthethird <simonzhang9518@gmail.com>
2021-08-18 18:24:17 -0700

    radded control form for signup

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,10 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function SignUpForm( {signup} ) {
+function SignUpForm({ signup }) {
+  const [signUpInfo, setSignUpInfo] = useState(
+    { username: "", password: "", firstName:"", lastName:"",email:"" });
 
-  const[signUpInfo, setSignUpInfo] = useState({username:"", password:""})
-
-  
   function handleChange(evt) {
     const { name, value } = evt.target;
     setSignUpInfo((SignUpData) => ({
@@ -19,59 +18,58 @@ function SignUpForm( {signup} ) {
     signup(signUpInfo);
   }
 
-
   return (
-    <div className="SignUpForm" style={{padding: "8px"}}>
-    <form className="Sign-up-page" onSubmit={handleSubmit}>
-      <input
-        style={{width: '400px'}}
-        id="Sign-up-username"
-        name="username"
-        className="form-control"
-        placeholder="Username"
-        onChange={handleChange}
-        value={signUpInfo.username}
-      />
-      <input
-        style={{width: '400px'}}
-        id="Sign-up-password"
-        name="password"
-        className="form-control"
-        placeholder="Password"
-        onChange={handleChange}
-        value={signUpInfo.password}
-      />
-      <input
-        style={{width: '400px'}}
-        id="Sign-up-first-name"
-        name="firstName"
-        className="form-control"
-        placeholder="First Name"
-        onChange={handleChange}
-        value={signUpInfo.firstName}
-      />
-      <input
-        style={{width: '400px'}}
-        id="Sign-up-last-name"
-        name="lastName"
-        className="form-control"
-        placeholder="Last Name"
-        onChange={handleChange}
-        value={signUpInfo.lastName}
-      />
-      <input
-        style={{width: '400px'}}
-        id="Sign-up-email"
-        name="email"
-        className="form-control"
-        placeholder="Email"
-        onChange={handleChange}
-        value={signUpInfo.email}
-      />
-      <button>Log In</button>
-    </form>
-  </div>
-  )
+    <div className="SignUpForm" style={{ padding: "8px" }}>
+      <form className="Sign-up-page" onSubmit={handleSubmit}>
+        <input
+          style={{ width: "400px" }}
+          id="Sign-up-username"
+          name="username"
+          className="form-control"
+          placeholder="Username"
+          onChange={handleChange}
+          value={signUpInfo.username}
+        />
+        <input
+          style={{ width: "400px" }}
+          id="Sign-up-password"
+          name="password"
+          className="form-control"
+          placeholder="Password"
+          onChange={handleChange}
+          value={signUpInfo.password}
+        />
+        <input
+          style={{ width: "400px" }}
+          id="Sign-up-first-name"
+          name="firstName"
+          className="form-control"
+          placeholder="First Name"
+          onChange={handleChange}
+          value={signUpInfo.firstName}
+        />
+        <input
+          style={{ width: "400px" }}
+          id="Sign-up-last-name"
+          name="lastName"
+          className="form-control"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={signUpInfo.lastName}
+        />
+        <input
+          style={{ width: "400px" }}
+          id="Sign-up-email"
+          name="email"
+          className="form-control"
+          placeholder="Email"
+          onChange={handleChange}
+          value={signUpInfo.email}
+        />
+        <button>Log In</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpForm;
